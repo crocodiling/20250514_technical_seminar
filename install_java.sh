6a4a2f94ae154d2b89778731020f2a109974f741
crocodiling <swings.chalky_0l@icloud.com>
2025-05-10 13:47:34 +0900

    Add files via upload

---
+++ install_java.sh
@@ -0,0 +1,2 @@
+brew install java #javaをインストール
+/opt/homebrew/cellar/openjdk/23.0.2/bin/java --version
---
+++ sample_java.sh
@@ -0,0 +1,12 @@
+cd ~/desktop #デスクトップに移動
+mkdir sample_java #デスクトップにsample_javaディレクトリを作成
+cd sample_java #sample_javaに移動
+touch sample.java #sample.javaを作成
+echo 'public class sample{
+    public static void main(String[] args){
+        System.out.println("正しくJavaが実行できています");
+    }
+}' >> sample.java #ファイルに書き込み
+
+/opt/homebrew/cellar/openjdk/23.0.2/bin/javac ~/desktop/sample_java/sample.java
+/opt/homebrew/cellar/openjdk/23.0.2/bin/java -cp ~/Desktop/sample_java sample    
